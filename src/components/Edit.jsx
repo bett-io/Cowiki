a73419343a1814178ddcd6f236ed6932980d539f
Young Han Lee <joybro201@gmail.com>
2018-01-16 00:31:00 +0000

    Add more components to edit page

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import { FormGroup, FormControl } from 'react-bootstrap';
+import { FormGroup, FormControl, Tabs, Tab, Button } from 'react-bootstrap';
 
 const Edit = (props) => {
   const { pageId } = props;
@@ -11,12 +11,21 @@ const Edit = (props) => {
     <div>
       <h1>Editing {pageId}</h1>
       <FormGroup>
-        <FormControl
-          type="text"
-          value={pageId}
-          placeholder="Enter title"
-        />
-        <FormControl componentClass="textarea" placeholder="textarea" />
+        <FormControl type="text" value={pageId} placeholder="Enter title" />
+        <br/>
+        <Tabs defaultActiveKey={1} id="write-preview">
+          <Tab eventKey={1} title="Write">
+            <FormControl componentClass="textarea" rows={15} />
+          </Tab>
+          <Tab eventKey={2} title="Preview">
+            Preview placeholder
+          </Tab>
+        </Tabs>
+        <br/>
+        <h5>Edit Message</h5>
+        <FormControl type="text" placeholder="Write a small message here explaining this change. (Optional)" />
+        <br/>
+        <Button className="pull-right" bsStyle="success">Save Page</Button>
       </FormGroup>
     </div>
   );
