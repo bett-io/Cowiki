827e0fdf67859d545f8a27986f0cde9fbe91dc84
Young Han Lee <joybro201@gmail.com>
2018-01-18 20:42:04 +0000

    Move article related logic to api file from db
    db files will have only db abstraction logic not related with business.

---
+++ server/apis/__tests__/article.test.js
@@ -0,0 +1,52 @@
+// @Flow
+
+import { create, read, update } from '../article'
+
+describe('article', () => {
+  test('read should return null for non-exist article request', () => {
+    expect.assertions(1);
+    return read('nonExist').then(result => {
+      expect(result).toBeNull()
+    });
+  });
+
+  test('create should insert the given article', () => {
+    expect.assertions(2);
+
+    return create('id1', 'hello world').then(ret => {
+      expect(ret).toBeTruthy();
+
+      return read('id1');
+    }).then(ret => {
+      expect(ret).toMatchObject({ id: 'id1', content: 'hello world' });
+    })
+  });
+
+  test('update should update article in db', () => {
+    expect.assertions(3);
+
+    return create('id2', 'hello world').then(result => {
+      expect(result).toBeTruthy();
+
+      return update('id2', 'hello world!!', 0);
+    }).then(result => {
+      expect(result).toBeTruthy();
+
+      return read('id2');
+    }).then(result => {
+      expect(result).toMatchObject({ id: 'id2', content: 'hello world!!' });
+    })
+  })
+
+  test('update should be failed when the given revision doesn\'t match', () => {
+    expect.assertions(2);
+
+    return create('id3', 'hello world').then(result => {
+      expect(result).toBeTruthy();
+
+      return update('id3', 'hello world!!', 1);
+    }).then(result => {
+      expect(result).toBeFalsy();
+    });
+  })
+});
