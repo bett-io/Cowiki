ff569068054ce00811349291709c52e25c0ace4f
Young Han Lee <joybro201@gmail.com>
2018-01-19 00:32:08 +0000

    Make save button work in Edit page

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -4,18 +4,41 @@ import React from 'react';
 
 import { FormGroup, FormControl, Tabs, Tab, Button } from 'react-bootstrap';
 
-const Edit = (props) => {
-  const { pageId } = props;
+export type EditProps = {
+  pageId: string,
+  content: string,
+  revision: number,
+  onSubmit: (string, number) => void,
+}
+
+const Edit = (props: EditProps) => {
+  const { pageId, content, revision, onSubmit } = props;
+  let title;
+  let updatedContent = content;
+
+  const handleSubmit = () => {
+    onSubmit(updatedContent, revision);
+  }
+
+  const handleTitleChange = (e) => {
+    title = e.target.value;
+  }
+
+  const handleContentChange = (e) => {
+    updatedContent = e.target.value;
+  }
 
   return (
     <div>
       <h1>Editing {pageId}</h1>
       <FormGroup>
-        <FormControl type="text" value={pageId} placeholder="Enter title" />
+        <FormControl type="text" value={pageId} placeholder="Enter title"
+          onChange={handleTitleChange} />
         <br/>
         <Tabs defaultActiveKey={1} id="write-preview">
           <Tab eventKey={1} title="Write">
-            <FormControl componentClass="textarea" rows={15} />
+            <FormControl componentClass="textarea" rows={15} defaultValue={updatedContent}
+              onChange={handleContentChange} />
           </Tab>
           <Tab eventKey={2} title="Preview">
             Preview placeholder
@@ -25,7 +48,7 @@ const Edit = (props) => {
         <h5>Edit Message</h5>
         <FormControl type="text" placeholder="Write a small message here explaining this change. (Optional)" />
         <br/>
-        <Button className="pull-right" bsStyle="success">Save Page</Button>
+        <Button className="pull-right" bsStyle="success" onClick={handleSubmit}>Save Page</Button>
       </FormGroup>
     </div>
   );
