8d5f4a77e425aacf60c3d65536b60acd5978200d
Young Han Lee <joybro201@gmail.com>
2018-01-20 14:07:18 +0000

    Fix some eslint violations

--- server/apis/article.js
+++ server/apis/article.js
@@ -5,15 +5,13 @@ import db from '../db/db';
 
 import type { Article } from '../db/db.flow';
 
-const file = 'server/apis/article.js';
+const create = async (id: string, content: string): boolean => db.createArticle({
+  id,
+  content,
+  rev: 0,
+});
 
-const create = async (id: string, content: string): boolean => {
-  return db.createArticle({ id, content, rev: 0 });
-}
-
-const read = async (id: string): ?Article => {
-  return db.readArticle({ id });
-}
+const read = async (id: string): ?Article => db.readArticle({ id });
 
 const update = async (id: string, content: string, currentRev: number): boolean => {
   const article = await read(id);
@@ -22,8 +20,8 @@ const update = async (id: string, content: string, currentRev: number): boolean 
 
   if (article.rev !== currentRev) return false;
 
-  return db.updateArticle(Object.assign({}, article, { content, rev: currentRev + 1 }))
-}
+  return db.updateArticle(Object.assign({}, article, { content, rev: currentRev + 1 }));
+};
 
 module.exports = {
   create,
