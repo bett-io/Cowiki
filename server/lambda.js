304d789c9d11e9b339489d21821c5edd5f021996
Young Han Lee <joybro201@gmail.com>
2018-01-24 23:39:40 +0000

    Move creating app instance logic out of server library
    This is a preperation of upcoming HMR patches. Setting up HMR should
    be done to express instance before setting up app routing. And
    shouldn't be included into prod bundle.

--- server/lambda.js
+++ server/lambda.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+const express = require('express');
+const setupServer = require('./server');
+
+const app = express();
+setupServer(app);
+
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
--- server/standalone.js
+++ server/standalone.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const app = require('./app');
+const express = require('express');
+const setupServer = require('./server');
+
+const app = express();
+setupServer(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
